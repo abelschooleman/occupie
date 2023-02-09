fd99dc128d859c322caa9f77c713a297e1592e98
Abel Schooleman <abel@onom.io>
2023-02-09 23:23:27 +0100

    refactor: construct month with date

--- app/Types/Month.php
+++ app/Types/Month.php
@@ -6,34 +6,33 @@ use Illuminate\Support\Carbon;
 
 class Month implements Period
 {
+    private Carbon $date;
+
     public function __construct(
         public readonly int $year,
         public readonly int $month
-    ) {}
+    ) {
+        $this->date = Carbon::create($this->year, $this->month);
+    }
 
     public function days(): int
     {
-        return $this->init()->daysInMonth;
+        return $this->date->daysInMonth;
     }
 
     public function end(): Carbon
     {
-        return $this->init()->endOfMonth();
+        return $this->date->endOfMonth();
     }
 
     public function start(): Carbon
     {
-        return $this->init()->startOfMonth();
+        return $this->date->startOfMonth();
     }
 
     public function toString(): string
     {
         return $this->year . '-' . $this->month;
-    }
-
-    private function init(): Carbon
-    {
-        return Carbon::create($this->year, $this->month);
     }
 
     public static function fromString(string $date): Month
